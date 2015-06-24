2889419b65ee4ad0d742af6fae68cb9823966a14
Andrew Moore <mooreandrewlr@gmail.com>
2015-06-24 18:25:32 +0100

    Jenkins Example Tests

--- tests/Example2/features/support/config.rb
+++ tests/Example2/features/support/config.rb
@@ -8,12 +8,27 @@ require 'capybara/cucumber'
 ### Allows you to use the page. commands
 include Capybara::DSL
 
-### Configures Capybara to use Xpath selectors and use poltergeist driver
+
 Capybara.default_selector = :xpath
 Capybara.default_wait_time = 10
-Capybara.app_host = 'http://localhost:4567' # change url
+Capybara.app_host = 'http://localhost:4567'
 
-require 'selenium-webdriver'
+Capybara.default_driver = :poltergeist
+Capybara.javascript_driver = :poltergeist
 
-Capybara.default_driver = :selenium
-Capybara.javascript_driver = :selenium
+if (ENV['webdriver'] == 'poltergeist') then
+
+  require 'capybara/poltergeist'
+
+  Capybara.register_driver :poltergeist do |app|
+    Capybara::Poltergeist::Driver.new(app, inspector: true, js_errors: true)
+  end
+
+else
+
+  require 'selenium-webdriver'
+
+  Capybara.default_driver = :selenium
+  Capybara.javascript_driver = :selenium
+
+end
